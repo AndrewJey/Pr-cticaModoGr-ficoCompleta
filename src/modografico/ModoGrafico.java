21066dffee74d8ef40cddf0e70667bd5ca6502e9
Andrés Jozef Jiménez Leandro <AndrewJey@gmail.com>
2016-05-13 23:13:43 -0600

    PrácticaModoGráficoCompleta
    PrácticaModoGráficoCompleta en Java

---
+++ src/modografico/ModoGrafico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modografico;
+/**
+ *
+ * @author Andrés
+ */
+public class ModoGrafico {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana vn = new Ventana();
+        vn.setVisible(true);
+    }   
+}
